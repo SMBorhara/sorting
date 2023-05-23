c34ef8280b476c1cea12fdf4aa7da5b7d54aeae9
Shivani Borhara <shivaniborhara@shivanis-mbp.lan>
2023-05-23 14:04:47 -0400

    sorting logic

--- build/index.js
+++ build/index.js
@@ -1,1 +1,8 @@
-console.log('hi there');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sorter_1 = require("./sorter");
+const NumbersCollections_1 = require("./NumbersCollections");
+const numbers = new NumbersCollections_1.NumbersCollections([10, 3, -5, 0]);
+const sorter = new sorter_1.Sorter(numbers);
+sorter.sort();
+console.log(numbers.data);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hi there')
+import { Sorter } from "./sorter";
+import { NumbersCollections } from "./NumbersCollections";
+
+const numbers = new NumbersCollections([10, 3, -5, 0])
+
+const sorter = new Sorter(numbers);
+sorter.sort();
+
+console.log(numbers.data);
